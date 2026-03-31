80429fffb89407544eefc3b6f3af16586788eb55
steph <SteOmi-Jr@github.com>
2026-03-31 15:19:27 -0400

    Added GameMain.java and Gamelogic.java these are skeletons. We will add onto these and will run everything. Separate from test demos and

---
+++ src/main/java/com/vague/checkersgame/GameLogic.java
@@ -0,0 +1,23 @@
+package com.vague.checkersgame;
+
+public class GameLogic {
+    //Import Board and Piece
+    private Board board;
+    private Piece piece;
+
+    // Game Logic Functions
+
+    //Program Start places pieces on empty Board
+    public void setUpInitialBoard()
+    {
+        //Skeleton
+    }
+
+    //Piece Move function
+
+    //Jump Function
+
+    //Switch Turn function
+
+    //
+}
---
+++ src/main/java/com/vague/checkersgame/GameMain.java
@@ -0,0 +1,51 @@
+package com.vague.checkersgame;
+
+//Imports
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+//Main
+public class GameMain extends Application {
+
+    //Import Board and GameLogic
+    private Board board;
+    private GameLogic gameLogic;
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        //Initialize Logic
+        gameLogic = new GameLogic();
+
+        //Create Board
+        board = new Board(8);
+        board.displayBoard();
+
+        //Initialize Pieces on Board
+        gameLogic.setUpInitialBoard();
+
+        //Root
+        BorderPane root = new BorderPane();
+        root.setCenter(board.gameBoard);
+
+        //Scene
+        Scene scene = new Scene(root);
+
+        //Stage
+        primaryStage.setTitle("Checkers Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    //Future Methods
+
+    //Game Launch
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
